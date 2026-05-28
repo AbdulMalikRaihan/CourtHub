52f2eda7a15653cee11aa21d6158a0be8f043ff9
AbdulMalikRaihan <malikraihan21@gmail.com>
2026-05-28 18:17:55 +0700

    awalan

---
+++ Hackaton/script.js
@@ -0,0 +1,9 @@
+// home
+
+const home = {
+    title: "Selamat Datang di Court-Hub",
+    description: "Kini kamu ga perlu pusing cari kawan mabar. Dapatkan teman dan lawan baru dengan mudah tiap minggunya hanya di Aplikasi Court-Hub.",
+    image: "/gambar/home.jpg"
+}
+
+localStorage.setItem("data_home",JSON.stringify(home));
